fbec56b1a0141b11285e3e06ddcacff24c8746a7
minhquoc5599 <minhquoc5599@gmail.com>
2024-09-09 15:01:10 +0700

    Add identity

---
+++ src/Blog.Data/BlogContext.cs
@@ -0,0 +1,49 @@
+﻿using Blog.Core.Domain.Content;
+using Blog.Core.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public BlogContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostCategory> PostCategories { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<PostActivityLog> PostActivityLogs { get; set; }
+        public DbSet<Series> Series { get; set; }
+        public DbSet<PostInSeries> PostInSeries { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims").HasKey(t => t.Id);
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("AppRoleClaims").HasKey(t => t.Id);
+            builder.Entity<IdentityUserLogin<Guid>>().ToTable("AppUserLogins").HasKey(t => t.UserId);
+            builder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(t => new { t.RoleId, t.UserId });
+            builder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens").HasKey(t => new { t.UserId });
+        }
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries().Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+            foreach (var entityEntry in entries)
+            {
+                var dateCreatedProp = entityEntry.Entity.GetType().GetProperty("DateCreated");
+                if (entityEntry.State == EntityState.Added && dateCreatedProp != null)
+                {
+                    dateCreatedProp.SetValue(entityEntry.Entity, DateTime.Now);
+                }
+
+                var modifiedDateProp = entityEntry.Entity.GetType().GetProperty("ModifiedDate");
+                if (entityEntry.State == EntityState.Modified && modifiedDateProp != null)
+                {
+                    modifiedDateProp.SetValue(entityEntry.Entity, DateTime.Now);
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
